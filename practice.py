b2b1b555cde332f9e746b0b22bfe4be674d8c9c4
DevSunn <changsun050971@gmail.com>
2022-02-19 05:06:50 +0900

    during 4-5

--- practice.py
+++ practice.py
@@ -148,8 +148,37 @@ print(python.count("n")) # 등장한 횟수
 # print("a", "b")
 
 # 방법 1
-print("나는 %d살입니다." % 20)
-print("나는 #s를 좋아해요." % "파이썬")
-print("Apple 은 %c로 시작해요." % "A")
+# print("나는 %d살입니다." % 20)
+# print("나는 #s를 좋아해요." % "파이썬")
+# print("Apple 은 %c로 시작해요." % "A")
+# %s
+# print("나는 %s살입니다.")
+# print("나는 %s색과 %s색을 좋아해요." % ("파란", "빨간"))
 
+# 방법 2
+# print("나는 {}살입니다.".format(20))
+# print("나는 {}색과 {}색을 좋아해요.".format("파란", "빨간"))
+# print("나는 {0}색과 {1}색을 좋아해요.".format("파란", "빨간"))
+# print("나는 {1}색과 {0}색을 좋아해요.".format("파란", "빨간"))
 
+# 방법 3
+# print("나는 {age}살이며, {color}색을 좋아해요.".format(age = 20, color="빨간"))
+# print("나는 {age}살이며, {color}색을 좋아해요.".format(color="빨간", age = 20))
+
+# 방법 4 (v3.6 이상~)
+# age = 20
+# color = "빨간"
+# print("나는 {age}살이며, {color}색을 좋아해요.")
+
+# 탈출 문자( \n : 줄바꿈)
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" \' : 문장 내에서 따옴표
+# 저는 "나도코딩"입니다.
+print("저는 '나도코딩'입니다.")
+print('저는 "나도코딩"입니다')
+print("저는 \"나도코딩\"입니다.")
+print("저는 \'나도코딩\'입니다.")
+
+# \\ : 문장 내에서 \
+print("C:\\Users\\Nadocoding\\Desktop\\PythonWorkspace>")
