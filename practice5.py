941f8a336f0001addd3bbb09cab824f0798b2efe
DevSunn <changsun050971@gmail.com>
2022-02-20 03:27:42 +0900

    during 6-1

--- practice5.py
+++ practice5.py
@@ -1,4 +1,4 @@
-# 5-1 리스트 []
+######### 5-1 리스트 []
 
 # 지하철 칸별로 10명, 20명, 30명
 # subway1 = 10
@@ -60,11 +60,102 @@
 # print(mix_list)
 
 
-# 5-2 사전
-cabinet = {3:"유재석", 100:"김태호"}
-print(cabinet[3])
-print(cabinet[100])
+######### 5-2 사전 
 
-print(cabinet.get(3))
-print(cabinet.get(5))
-print("hi")
+# cabinet = {3:"유재석", 100:"김태호"}
+# print(cabinet[3])
+# print(cabinet[100])
+
+# print(cabinet.get(5))
+# print(cabinet.get(5, "사용 가능"))
+# print("hi")
+
+# print(3 in cabinet) # True
+# print(5 in cabinet) # False
+
+# cabinet = {"A-3":"유재석", "B-100":"김태호"}
+# print(cabinet["A-3"])
+# print(cabinet["B-100"])
+
+# # 새 손님
+# print(cabinet)
+# cabinet["A-3"] = "김종국"
+# cabinet["C-20"] = "조세호"
+# print(cabinet)
+
+# # 간 손님
+# del cabinet["A-3"]
+# print(cabinet)
+
+# # key 들만 출력
+# print(cabinet.keys())
+
+# # value 들만 출력
+# print(cabinet.values())
+
+# # key, value 쌍으로 출력
+# print(cabinet.itmes())
+
+# # 목욕탕 폐점
+# cabinet.clear() # 모든 값 없어짐
+# print(cabinet)
+
+######### 5-3 튜플
+# menu = ("돈까스", "치즈까스")
+# print(menu[0])
+# print(menu[1])
+
+#menu.add("생선까스")
+
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+# (name, age, hobby) = ("김종국", 20, "코딩")
+# print(name, age, hobby)
+
+
+######### 5-4 세트
+# 집합 (set)
+# 중복 안됨, 순서 없음
+# my_set = {1,2,3,3,3}
+# print(my_set)
+
+# java = {"유재석", "김태호", "양세형"}
+# python = set(["유재석", "박명수"])
+
+# # 교집합 (java 와 python 을 모두 할 수 있는 개발자)
+# print(java & python)
+# print(java.intersection(python))
+
+# # 합집합 (java 도 할 수 있거나 python 할 수 있는 개발자)
+# print(java | python)
+# print(java.union(python))
+
+# # 차집합 (java 할 수 있지만 python 은 할 줄 모르는 개발자)
+# print(java - python)
+# print(java.difference(python))
+
+# # python 할 줄 아는 사람이 늘어남
+# python.add("김태호")
+# print(python)
+
+# # java 를 잊었어요
+# java.remove("김태호")
+# print(java)
+
+
+######### 5-5 자료구조의 변경
+# 커피숍
+# menu = {"커피", "우유", "주스"}
+# print(menu, type(menu))
+
+# menu = list(menu)
+# print(menu, type(menu))
+
+# menu = tuple(menu)
+# print(menu, type(menu))
+
+# menu = set(menu)
+# print(menu, type(menu))
---
+++ practice6.py
@@ -0,0 +1,4 @@
+######## 6-1 if
+weather = "비"
+if weather == "비" :
+    print("우산을 챙기세요")
